83399c43e463522ff0569a826a7f22e59b5df8d7
Steve Purves <steve@curvenote.com>
2024-06-07 22:47:26 +0100

    Update renderer.tsx

--- src/renderer.tsx
+++ src/renderer.tsx
@@ -8,5 +8,7 @@ export type ITKView2dDirective = {
 
 export const ITKView2dRenderer: NodeRenderer = ({ node }: { node: ITKView2dDirective }) => {
   // TODO add html, custom tag and inject js
+  // NOTE this file cannot be added to the plugin yet, but we should write it and we can
+  // add it to a theme build directly
   return <div>ITK View 2d</div>;
 };
